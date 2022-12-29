90e98629d8d9de283cf91eadbb54b2f4463ef834
chan <imchrg1@gmail.com>
2022-12-29 11:56:42 +0900

    Feature: 레이아웃 수정 및 코드 정리

--- App.tsx
+++ App.tsx
@@ -8,145 +8,69 @@
  * @format
  */
 
-import React, {useRef, type PropsWithChildren} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
   NativeSyntheticEvent,
   NativeScrollEvent,
   Animated,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type AnimatedHeaderProps = {
-  height: Animated.Value | Animated.AnimatedInterpolation;
-};
-
-const AnimatedHeader = ({height}: AnimatedHeaderProps) => {
-  return (
-    <Animated.View style={{...styles.animatedHeader, height}}>
-      <Text>Animated Header</Text>
-    </Animated.View>
-  );
-};
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const scrolling = useRef(new Animated.Value(0)).current;
+  const scrolling = React.useRef(new Animated.Value(0)).current;
   const height = scrolling.interpolate({
     inputRange: [0, 200],
-    outputRange: [100, 50],
+    outputRange: [200, 50],
+    extrapolate: 'clamp',
+  });
+  const fontSize = scrolling.interpolate({
+    inputRange: [0, 200],
+    outputRange: [32, 16],
     extrapolate: 'clamp',
   });
 
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   const onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    console.log(scrolling);
-
     scrolling.setValue(e.nativeEvent.contentOffset.y);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AnimatedHeader height={height} />
+    <SafeAreaView>
+      <Animated.View style={{...styles.animatedHeaderContainer, height}}>
+        <Animated.Text style={{...styles.animatedHeaderText, fontSize}}>Animated Header</Animated.Text>
+      </Animated.View>
       <Animated.ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
         onScroll={onScroll}
         scrollEventThrottle={10}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        {Array(20).fill(0).map((_, index) => {
+          return (
+            <View key={index} style={styles.listItemContainer}>
+              <Text style={{color: 'black'}}>{`item${index}`}</Text>
+            </View>
+          );
+        })}
       </Animated.ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  animatedHeader: {
+  animatedHeaderContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'orange',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  animatedHeaderText: {
+    color: 'black',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  listItemContainer: {
+    height: 100,
+    marginVertical: 10,
+    backgroundColor: 'lightgray',
   },
 });
 
